e87a78d4577582947d32b7978e98f878915c3bfc
AdiSuba <adi_subasic@hotmail.com>
2016-04-21 11:36:43 +0200

    Upgrade Battle Meni-ja

--- Projekat/RolePlayingGame/RolePlayingGame/BattleMeni.xaml.cs
+++ Projekat/RolePlayingGame/RolePlayingGame/BattleMeni.xaml.cs
@@ -26,11 +26,30 @@ namespace RolePlayingGame
         public BlankPage1()
         {
             this.InitializeComponent();
+            Confirm.Opacity = 0;
+            Confirm_Image.Opacity = 0;
+            Deny.Opacity = 0;
+            Deny_Image.Opacity = 0;
+            Attack_Select.Opacity = 0;
+            Ability_Select.Opacity = 0;
+            Item_Select.Opacity = 0;
+            Run_Select.Opacity = 0;
         }
-
-        int k = 0;
+        private void ResetSelect()
+        {
+            Attack_Select.Opacity = 0;
+            Ability_Select.Opacity = 0;
+            Item_Select.Opacity = 0;
+            Run_Select.Opacity = 0;
+        }
+        int k = 0;  //k - parametar koji pamti koja su dugmad pritisnuta i njihov redoslijed
+                    //1. cifra - zadnje pritisnuto dugme, 3. cifra prvo pritisnuto dugme
+        int p = 0;  //p - parametar koji pamti koliko je puta dugme pritisnuto
+                    //p = 1 ili 0, nije ni jednom pritisnuto, 10 jednom pritisnuto, ..., 1000 tri puta pritisnuto
         private void Attack_Click(object sender, RoutedEventArgs e)
         {
+            ResetSelect();
+            Attack_Select.Opacity = 1;
             switch (k)
             {
                 case 0:
@@ -38,16 +57,18 @@ namespace RolePlayingGame
                     Confirm_Image.Opacity = 1;
                     Deny.Opacity = 1;
                     Deny_Image.Opacity = 1;
-                    k = 1;
+                    k = 1; p = 10;
                     break;
                 default:
-                    k = 10 * k + 1;
+                    k = p + k % p;
                     break;
             }
         }
 
         private void Use_Ability_Click(object sender, RoutedEventArgs e)
         {
+            ResetSelect();
+            Ability_Select.Opacity = 1;
             switch (k)
             {
                 case 0:
@@ -55,16 +76,18 @@ namespace RolePlayingGame
                     Confirm_Image.Opacity = 1;
                     Deny.Opacity = 1;
                     Deny_Image.Opacity = 1;
-                    k = 2;
+                    k = 2; p = 10;
                     break;
                 default:
-                    k = 10 * k + 2;
+                    k = 2 * p + k % p;
                     break;
             }
         }
 
         private void Use_Item_Click(object sender, RoutedEventArgs e)
         {
+            ResetSelect();
+            Item_Select.Opacity = 1;
             switch (k)
             {
                 case 0:
@@ -72,16 +95,18 @@ namespace RolePlayingGame
                     Confirm_Image.Opacity = 1;
                     Deny.Opacity = 1;
                     Deny_Image.Opacity = 1;
-                    k = 3;
+                    k = 3; p = 10;
                     break;
                 default:
-                    k = 10 * k + 3;
+                    k = 3 * p + k % p;
                     break;
             }
         }
 
         private void Run_Away_Click(object sender, RoutedEventArgs e)
         {
+            ResetSelect();
+            Run_Select.Opacity = 1;
             switch (k)
             {
                 case 0:
@@ -89,80 +114,90 @@ namespace RolePlayingGame
                     Confirm_Image.Opacity = 1;
                     Deny.Opacity = 1;
                     Deny_Image.Opacity = 1;
-                    k = 4;
+                    k = 4; p = 1;
                     break;
                 default:
-                    k = 10 * k + 4;
+                    k = 4 * p + k % p;
                     break;
             }
         }
 
         private void Confirm_Click(object sender, RoutedEventArgs e)
         {
-            switch (k)
+            ResetSelect();
+            switch (k % (p * 10))
             {
                 case 1:
-                    Attack.Content = "Attack1";
-                    Use_Ability.Content = "Attack2";
-                    Use_Item.Content = "Attack3";
-                    Run_Away.Content = "Attack4";
+                    Attack_Text.Text = "Attack1" + k.ToString();
+                    Ability_Text.Text = "Attack2";
+                    Item_Text.Text = "Attack3";
+                    Run_Text.Text = "Attack4";
+                    p = 10;
                     break;
                 case 2:
-                    Attack.Content = "Ability1";
-                    Use_Ability.Content = "Ability2";
-                    Use_Item.Content = "Ability3";
-                    Run_Away.Content = "Ability4";
+                    Attack_Text.Text = "Ability1";
+                    Ability_Text.Text = "Ability2";
+                    Item_Text.Text = "Ability3";
+                    Run_Text.Text = "Ability4";
+                    p = 10;
                     break;
                 case 4:
-                    Attack.Content = "You coward!";
-                    Use_Ability.Content = "";
-                    Use_Item.Content = "";
-                    Run_Away.Content = "";
+                    Attack_Text.Text = "You coward!";
+                    Ability_Text.Text = "";
+                    Item_Text.Text = "";
+                    Run_Text.Text = "";
+                    p = 10;
                     break;
                 case 11:
-                case 12:
-                case 13:
-                case 14:
                 case 21:
+                case 31:
+                case 41:
+                case 12:
                 case 22:
-                case 23:
-                case 24:
-                    Attack.Content = "Enemy1";
-                    Use_Ability.Content = "Enemy2";
-                    Use_Item.Content = "Enemy3";
-                    Run_Away.Content = "";
+                case 32:
+                case 42:
+                    Attack_Text.Text = "Enemy1";
+                    Ability_Text.Text = "Enemy2";
+                    Item_Text.Text = "Enemy3";
+                    Run_Text.Text = "";
+                    p = 100;
                     break;
                 default:
                     Player.Text = k.ToString();
-                    Attack.Content = "Attack";
-                    Use_Ability.Content = "Use Ability";
-                    Use_Item.Content = "Use Item";
-                    Run_Away.Content = "Run Away";
+                    Attack_Text.Text = "Attack";
+                    Ability_Text.Text = "Use Ability";
+                    Item_Text.Text = "Use Item";
+                    Run_Text.Text = "Run Away";
                     Confirm.Opacity = 0;
                     Confirm_Image.Opacity = 0;
                     Deny.Opacity = 0;
                     Deny_Image.Opacity = 0;
-                    k = 0;
+                    k = 0; p = 0;
                     break;
             }
         }
 
         private void Deny_Click(object sender, RoutedEventArgs e)
         {
-            k = k / 10;
-            switch (k)
+            ResetSelect();
+            p = p / 10;
+            k = k % 10;
+            if (p == 1) k = 0;
+            switch (k % (p * 10))
             {
                 case 1:
-                    Attack.Content = "Attack1";
-                    Use_Ability.Content = "Attack2";
-                    Use_Item.Content = "Attack3";
-                    Run_Away.Content = "Attack4";
+                    Attack_Text.Text = "Attack1";
+                    Ability_Text.Text = "Attack2";
+                    Item_Text.Text = "Attack3";
+                    Run_Text.Text = "Attack4";
+                    p = 10;
                     break;
                 case 2:
-                    Attack.Content = "Ability1";
-                    Use_Ability.Content = "Ability2";
-                    Use_Item.Content = "Ability3";
-                    Run_Away.Content = "Ability4";
+                    Attack_Text.Text = "Ability1";
+                    Ability_Text.Text = "Ability2";
+                    Item_Text.Text = "Ability3";
+                    Run_Text.Text = "Ability4";
+                    p = 10;
                     break;
                 case 11:
                 case 12:
@@ -172,22 +207,23 @@ namespace RolePlayingGame
                 case 22:
                 case 23:
                 case 24:
-                    Attack.Content = "Enemy1";
-                    Use_Ability.Content = "Enemy2";
-                    Use_Item.Content = "Enemy3";
-                    Run_Away.Content = "";
+                    Attack_Text.Text = "Enemy1";
+                    Ability_Text.Text = "Enemy2";
+                    Item_Text.Text = "Enemy3";
+                    Run_Text.Text = "";
+                    p = 100;
                     break;
                 default:
                     Player.Text = k.ToString();
-                    Attack.Content = "Attack";
-                    Use_Ability.Content = "Use Ability";
-                    Use_Item.Content = "Use Item";
-                    Run_Away.Content = "Run Away";
+                    Attack_Text.Text = "Attack";
+                    Ability_Text.Text = "Use Ability";
+                    Item_Text.Text = "Use Item";
+                    Run_Text.Text = "Run Away";
                     Confirm.Opacity = 0;
                     Confirm_Image.Opacity = 0;
                     Deny.Opacity = 0;
                     Deny_Image.Opacity = 0;
-                    k = 0;
+                    k = 0; p = 0;
                     break;
             }
         }
@@ -207,5 +243,9 @@ namespace RolePlayingGame
 
         }
 
+        private void textBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
